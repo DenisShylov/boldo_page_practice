d7c1a899207fbb7ac5ec0cccb24626c0a0510d5e
DenisShylov <dodjer123@gmail.com>
2023-12-13 17:28:34 +0200

    feat: added ticker

--- src/Components/LogoTicker/LogoTicker.jsx
+++ src/Components/LogoTicker/LogoTicker.jsx
@@ -1,72 +1,85 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
 import { BoldoLogo, PrestoLogo, LogoText } from './LogoTicker.styles';
+import Marquee from 'react-fast-marquee';
 
 const LogoTicker = () => {
+  const theme = useTheme();
+  const screenSize = useMediaQuery(theme.breakpoints.down('sm'));
+  const conditionForGradientSize = screenSize ? 75 : 200;
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        height: '93px',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background:
-          'linear-gradient(90deg, #0A2640 0%, rgba(10, 38, 64, 0.11) 31.6%, rgba(10, 38, 64, 0.00) 72.89%, #0A2640 97.87%)',
-      }}
+    <Marquee
+      speed={100}
+      gradient={true}
+      gradientColor="#0A2640, #0A2640"
+      gradientWidth={conditionForGradientSize}
+      autoFill
     >
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        {[1, 2, 3].map((item) => (
-          <>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'baseline',
-                gap: {
-                  xs: 0,
-                  sm: '10px',
-                  md: '10px',
-                  lg: '10px',
-                  xl: '10px',
-                },
-                mr: {
-                  xs: '10px',
-                  sm: '20px',
-                  md: '20px',
-                  lg: '40px',
-                  xl: '40px',
-                },
-              }}
-            >
-              <BoldoLogo />
-              <LogoText variant={'span'}>Boldo</LogoText>
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'baseline',
-                gap: {
-                  xs: 0,
-                  sm: '10px',
-                  md: '10px',
-                  lg: '10px',
-                  xl: '10px',
-                },
-                mr: {
-                  xs: '10px',
-                  sm: '20px',
-                  md: '20px',
-                  lg: '40px',
-                  xl: '40px',
-                },
-              }}
-            >
-              <PrestoLogo />
-              <LogoText variant={'span'}>Presto</LogoText>
-            </Box>
-          </>
-        ))}
+      <Box
+        sx={{
+          display: 'flex',
+          height: '93px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background:
+            'linear-gradient(90deg, #0A2640 0%, rgba(10, 38, 64, 0.11) 31.6%, rgba(10, 38, 64, 0.00) 72.89%, #0A2640 97.87%)',
+        }}
+      >
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          {[1, 2, 3].map((item) => (
+            <>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'baseline',
+                  gap: {
+                    xs: 0,
+                    sm: '10px',
+                    md: '10px',
+                    lg: '10px',
+                    xl: '10px',
+                  },
+                  mr: {
+                    xs: '10px',
+                    sm: '20px',
+                    md: '20px',
+                    lg: '40px',
+                    xl: '40px',
+                  },
+                }}
+              >
+                <BoldoLogo />
+                <LogoText variant={'span'}>Boldo</LogoText>{' '}
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'baseline',
+                  gap: {
+                    xs: 0,
+                    sm: '10px',
+                    md: '10px',
+                    lg: '10px',
+                    xl: '10px',
+                  },
+                  mr: {
+                    xs: '10px',
+                    sm: '20px',
+                    md: '20px',
+                    lg: '40px',
+                    xl: '40px',
+                  },
+                }}
+              >
+                <PrestoLogo />
+                <LogoText variant={'span'}>Presto</LogoText>
+              </Box>
+            </>
+          ))}
+        </Box>
       </Box>
-    </Box>
+    </Marquee>
   );
 };
 
